c60227869a817f443a8aaa4657351cc2ad292673
bellDev-code <sumaoo20@naver.com>
2023-10-13 22:58:26 +0900

    Camera

--- src/index.js
+++ src/index.js
@@ -8,73 +8,49 @@ if (WEBGL.isWebGLAvailable()) {
 
   // 렌더러
   const renderer = new THREE.WebGLRenderer({
+    alpha: true,
     antialias: true 
   })
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement);
 
   // 카메라
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  camera.position.z = 3
+  const fov = 80; // 화각 8-28이하 망원(확대), 화각 47 평범, 화각 63이상 광각(멀리)
+  const aspect = window.innerWidth / window.innerHeight; // 종횡비 
+  const near = 0.1; // 카메라의 시점이 시작되는 위치
+  const far = 1000; // 카메라의 시점이 끝나는 위치
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+  // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  // camera.position.set(0, 0, 1)
+  camera.position.x = 2;
+  camera.position.y = 2;
+  camera.position.z = 1;
+  camera.lookAt(new THREE.Vector3(0,0,0))
 
-
-  // 빛 추가
-  const pointLight = new THREE.PointLight(0xffffff, 1)
-  pointLight.position.set(0, 2, 12)
-  scene.add(pointLight)
-
-  // 텍스쳐 추가
-  const textureLoader = new THREE.TextureLoader()
-  const textBasicColor = textureLoader.load('../static/textures/Gravel_001_BaseColor.jpg')
-  const textNormalColor = textureLoader.load('../static/textures/Gravel_001_Normal.jpg')
-  const textHeightColor = textureLoader.load('../static/textures/Gravel_001_Height.png')
-  const textRoughenessColor = textureLoader.load('../static/textures/Gravel_001_Roughness.jpg')
 
   // 도형 추가
-  const geometry = new THREE.SphereGeometry(0.3, 32, 16)
-  const material = new THREE.MeshStandardMaterial({
-    map: textBasicColor
-  })
+  const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+  const material = new THREE.MeshStandardMaterial({color: 0xFFA500})
 
-  const object = new THREE.Mesh(geometry, material)
-  object.position.x = -2
-  scene.add(object)
+  const cube = new THREE.Mesh(geometry, material)
+  cube.rotation.y = 0.5;
+  
+  scene.add(cube)
 
-  const geometry02 = new THREE.SphereGeometry(0.3, 32, 16)
-  const material02 = new THREE.MeshStandardMaterial({
-    map: textBasicColor,
-    normalMap: textNormalColor
-  })
+  // 바닥 추가
+  const planeGeometry = new THREE.PlaneGeometry(30, 30, 1, 1)
+  const planeMaterial = new THREE.MeshStandardMaterial({color: 0xeeeeee})
 
-  const object02 = new THREE.Mesh(geometry02, material02)
-  object02.position.x = -1
-  scene.add(object02)
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.rotation.y = -0.2
 
-  const geometry03 = new THREE.SphereGeometry(0.3, 32, 16)
-  const material03 = new THREE.MeshStandardMaterial({
-    map: textBasicColor,
-    normalMap: textNormalColor,
-    displacementMap: textHeightColor,
-    displacementScale: 0.1
-  })
+  scene.add(plane)
 
-  const object03 = new THREE.Mesh(geometry03, material03)
-  object03.position.x = 0
-  scene.add(object03)
-
-  const geometry04 = new THREE.SphereGeometry(0.3, 32, 16)
-  const material04 = new THREE.MeshStandardMaterial({
-    map: textBasicColor,
-    normalMap: textNormalColor,
-    displacementMap: textHeightColor,
-    displacementScale: 0.1,
-    roughnessMap: textRoughenessColor,
-    roughness: 1
-  })
-
-  const object04 = new THREE.Mesh(geometry04, material04)
-  object04.position.x = 1
-  scene.add(object04)
+  // 빛 추가
+  const pointLight = new THREE.PointLight(0xffffbb, 1)
+  pointLight.position.set(0, 2, 12)
+  scene.add(pointLight)
 
   function render(time) {
     time *= 0.001
